160304b5003ace28c33c1c3d226fc4547187c66a
Alberto Rolle <alberto.rolleortega@gmail.com>
2026-03-01 03:57:23 +0100

    style: wip adapting styles

--- src/ui/Grid/Grid.tsx
+++ src/ui/Grid/Grid.tsx
@@ -7,11 +7,18 @@ interface GridProps<T> {
   renderItem: (item: T) => React.ReactNode;
   ariaLabel: string;
   keyExtractor: (item: T, key: number) => string;
+  variant?: 'default' | 'extended';
 }
 
-export const Grid = <T,>({ items, renderItem, ariaLabel, keyExtractor }: GridProps<T>) => {
+export const Grid = <T,>({
+  items,
+  renderItem,
+  ariaLabel,
+  keyExtractor,
+  variant = 'default',
+}: GridProps<T>) => {
   return (
-    <ul className={styles.grid} aria-label={ariaLabel}>
+    <ul className={styles.grid} aria-label={ariaLabel} data-variant={variant}>
       {items.map((item, key) => (
         <li key={keyExtractor(item, key)} className={styles.item}>
           {renderItem(item)}
